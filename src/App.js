0a000162a1b8ea7dd9a9f497351842b652829a88
mdhabibur <mdhrahman532@gmail.com>
2024-02-02 17:39:47 +0600

    new code pushed in branch 2.4

--- src/App.js
+++ src/App.js
@@ -1,114 +1,26 @@
 import {useState} from "react"
 import Counter from "./components/Counter";
-import Stats from "./components/Stats";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 
-const initialState = [
-  {
-    id:1,
-    count: 0,
-  },
-  {
-    id:2,
-    count:0,
-  }
-]
+//need to wrap the root 'App' component with Provider from redux so that whole application can access the redux state
 
 
+export default function App(){
+  return (
+    <Provider store={store}>
+      <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
 
+        <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+            Simple Counter Application
+        </h1>
 
-export default function App() {
+        <div className="max-w-md mx-auto mt-10 space-y-5">
+            <Counter id="1" />
+        </div>
+     </div>
 
-  const [state, setState] = useState(initialState);
-
-  const totalCount = () => {
-    return state.reduce((total,counter) => total + counter.count,0)
-  }
-
-  const increment = (id) => {
-    //we will never directly mutate the 'state' object
-    //we will use 'immutably state change' technique
-    //we will first make a clone of state, change where necessary and then return
-    //updated state that will replace the previous state
-
-    const updatedState = state.map((c) => {
-
-      if(c.id === id){
-        return {
-          ...c,
-          count: c.count + 1,
-        }
-
-      }
-
-      return {...c}
-
-    })
-
-    setState(updatedState)
-    /*
-
-    map() function calls all counter objects one by one, checks if the 'passed id'
-    matches any counter object's is, if matches then first makes a copy of the counter object then updates the 'count' property and at last returns the 
-    'updated counter' object to the new 'updatedState' array.
-
-    if condition does not match, it still makes a copy of that original counter
-    object and returns it to the new array.
-
-    so the 'previous state' of the counter arrays , are totally replaced by the
-    'new updated state' of the new arrays of the counter objects.
-
-    
-    */
-
-
-
-
-  }
-
-
-  const decrement = (id) => {
-
-    const updatedState = state.map((c) => {
-      if(c.id === id){
-        return {
-          ...c,
-          count : c.count - 1
-        }
-      }
-      return {...c}
-
-    })
-
-    setState(updatedState)
-
-  }
-
-
-	return (
-
-		<div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-			<h1 class="max-w-md mx-auto text-center text-2xl font-bold">
-				Simple Counter Application
-			</h1>
-
-      <div class="max-w-md mx-auto mt-10 space-y-5">
-
-        {state.map((count) => <Counter
-              key={count.id}
-              id={count.id}
-              count={count.count}
-              increment={increment}
-              decrement={decrement}
-              /> )}
-
-       <Stats count={totalCount()} />
-
-      </div>
-      
-
-
-		</div>
-	);
+    </Provider>
+  )
 }
-
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,56 @@
-import Button from "./Button";
-import Count from "./Count";
 
-export default function Counter({id, count, increment, decrement}) {
+//using connect() api to access redux store and access state and actions when using react class components 
+
+import { connect } from "react-redux";
+import { increment, decrement } from "../redux/counter/actions";
+
+function Counter({count, increment, decrement}) {
 	return (
 		<div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
 
-            <Count count={count} />
-			<div class="flex space-x-3">
-                <Button handler={() => increment(id)}>Increment</Button>
-                <Button handler={() => decrement(id)}>Decrement</Button>
+            <div className="text-2xl font-semibold">{count}</div>
 
-			</div>
+            <div className="flex space-x-3">
+                <button
+                    className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+                    onClick={increment}
+                >
+                    Increment
+                </button>
+                <button
+                    className="bg-red-400 text-white px-3 py-2 rounded shadow"
+                    onClick={decrement}
+                >
+                    Decrement
+                </button>
+            </div>
 
 		</div>
 	);
 }
+
+const mapStateToProps = (state, ownProps) => {
+	console.log(ownProps)
+	//this functions call also take the own props of Counter 
+	return {
+		count: state.value,
+		//here we are getting the state from redux
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		increment: (value) => dispatch(increment(5)),
+		decrement: (value) => dispatch(decrement(2)),
+
+
+	}
+}
+
+
+
+//connect() is a higher order component function (HOC) which takes an original component and returns another component
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
+//here mapStateToProps function is a call back function which will be passed to redux and redux will call it and will pass the 'state' through its argument
+
+//similarly mapDispatchToProps is also a callback function through which redux will pass the 'actions' to us
