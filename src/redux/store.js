8b7b8bd772fd03c03826bd0b8a91f6eab2fde42b
mdhabibur <mdhrahman532@gmail.com>
2024-02-02 22:53:11 +0600

    adding multiple middlewares

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import counterReducer from "./counter/counterReducer";
 import rootReducer from "./rootReducer";
+import myLogger from "./middlewares/myLogger";
+import logger from "redux-logger";
 
 
 /*
@@ -39,47 +41,13 @@ this is the work of middleware
 so middleware catches the 'action' before it is passed to 'reducer function' to change to corresponding state.
 
 
-
-
 */
 
 
-const myLogger = (store) => (next) => (action) => {
-    //so this is a 3 steps carried function 
-    //and here we can access 'store' object, 'next()', and 'action' object
-
-    //as long as next() function is called, 'action' will not be passed to the 'reducer' function and 'redux' will not be able to call the 'reducer function' to update the state 
-
-    console.log(`Action: ${JSON.stringify(action)}`)
-    console.log(`previous state: ${JSON.stringify(store.getState())}`)
-
-
-
-
-
-    //till now though we clicked the 'increment button' the value of the counted is not updated , as redux could not be able to call 'reducer function' as 'action' was not passed to 'reducer function'
-
-    //but if we still want to see the 'upcoming state' we can use array.reduce() method and pass the action to update the state by ourself not by 'redux's reducer function, it is not called yet"
-
-    const upcomingState = [action].reduce(rootReducer, store.getState());
-
-    console.log(`Upcoming State: ${JSON.stringify(upcomingState)}`);
-
-
-    //now if we pass the action to the reducer function , only then the value of the counter will be updated
-
-    return next(action)
-
-
-
-
-
-
-}
 
 
 //as createStore() method can take only one reducer function
-const store = createStore(rootReducer, applyMiddleware(myLogger))
+const store = createStore(rootReducer, applyMiddleware(logger,myLogger))
 
 
 
