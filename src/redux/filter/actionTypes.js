064b7017b98b691c942d9ff6193bfdcd68bdfa28
mdhabibur <mdhrahman532@gmail.com>
2024-02-13 16:39:55 +0600

    redux toolkit RTK

--- src/redux/filter/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const FILTER_BY_STATUS = "filter/filteringByStatus"
-export const FILTER_BY_COLOR = "filter/filteringByColor"
--- src/redux/todos/actionTypes.js
+++
@@ -1,8 +0,0 @@
-
-export const ADD_TODO = "todo/addTodo";
-export const LOADED_TODOS = "todos/loaded"
-export const TOGGLE_COMPLETED = "todo/CompleteOrIncomplete";
-export const COLOR_SELECTED = "todo/colorSelected";
-export const DELETE_TODO = "todo/deleteTodo";
-export const COMPLETE_ALL_TODOS = "todo/completeAllTodos";
-export const CLEAR_COMPLETED = "todo/clearCompletedTodos";
